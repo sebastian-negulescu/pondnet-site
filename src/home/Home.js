71af1cb778b86295c57df6eeaad4e1a3e3df6dc1
Sebastian Negulescu <sebastian.a.negulescu@gmail.com>
2021-09-18 12:38:08 -0400

    added report pond form

--- src/home/Home.js
+++ src/home/Home.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Home = (props) => {
     return (
-        <div>
+        <>
             <button onClick={props.findClick}>find</button>
             <button onClick={props.reportClick}>report</button>
-        </div>
+        </>
     );
 };
 
--- src/report-pond/ReportPond.js
+++ src/report-pond/ReportPond.js
@@ -1,10 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ReportPond = () => {
+    const [rating, setRating] = useState(3);
+    const [locLat, setLocLat] = useState(0);
+    const [locLong, setLocLong] = useState(0);
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const res = await fetch('http://localhost:8000/', {
+                method: 'POST',
+                body: JSON.stringify({ 
+                    locLat: locLat,
+                    locLong: locLong,
+                    rating: rating 
+                })
+            });
+            if (!res.ok) {
+                const errorMessage = await res.text();
+		        throw new Error(errorMessage);
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
     return (
-        <div>
-            Report
-        </div>
+        <>
+            <h1>Report a Pond</h1>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    location:
+                    <input type='number' 
+                        value={locLat}
+                        onChange={e => setLocLat(e.target.value)} />
+                    <input type='number' 
+                        value={locLong}
+                        onChange={e => setLocLong(e.target.value)} />
+                </label>
+                <label>
+                    ice condition:
+                    <input type='number' 
+                        min='1' 
+                        max='5' 
+                        value={rating}
+                        onChange={e => setRating(e.target.value)} />
+                </label>
+                <input type='submit' value='submit'/>
+            </form>
+        </>
     );
 };
 
